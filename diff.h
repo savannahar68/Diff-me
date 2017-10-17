92d5677fa62ae318996de2db4822c3ca2c6cb940
Savan Nilesh Nahar <savannahar68@gmail.com>
2017-10-17 12:17:17 +0000

    Upload New File

---
+++ diff.h
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct file_data{
+	char **lines;
+	int *origlinenumber;
+	int totallines;
+
+}file_data;
+
+void initfiledata(file_data *a);
+int readlinesfromfiles(char *filename, file_data *a);
