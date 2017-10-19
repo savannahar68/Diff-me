7445e5db7a736a30c6c85b88c88523efaaf39e1c
Savan Nilesh Nahar <savannahar68@gmail.com>
2017-10-19 19:52:21 +0000

    Upload New File

---
+++ patch/patch.h
@@ -0,0 +1,19 @@
+typedef struct node{
+	char *line;
+	int pos;//storing position of line
+	int flag;//check deletion
+	struct node *next;
+}node;
+
+typedef struct filestore{
+	node *head;	
+	node *tail;
+}filestore;
+
+void init(filestore *f);
+void append(filestore *f, char *line, int pos);
+int insert(filestore *f, char *line, int pos);
+int deleteset(filestore *f, int pos);
+char *read(filestore *f);
+
+
